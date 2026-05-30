a56769acbaec5392887f930a1319afd0d3069dbc
Eduardo Galdos <egaldosar@unsa.edu.pe>
2026-05-30 12:50:40 -0500

    correcion, el contraste de los colores dependera de la profundidad total asegurando un mejor coloreado

--- examples/testLine.c
+++ examples/testLine.c
@@ -32,11 +32,18 @@ void CurvaDeLevy(Turtle* t, float length, int depth){
 
 }
 
-void ArbolFractalColoreado(Turtle* t, float length, int depth){
-  if(depth == 0 || length < 5 || 0 + 35 * depth > 255 || 255  - 35 * depth < 0)
+void ArbolFractalColoreadoHelper(Turtle* t, float length, int depth){
+  if(depth == 0)
+    return;
+  int contraste = 255 / depth;
+  
+  ArbolFractalColoreadoHelper(t, length, depth, contraste);
+}
+void ArbolFractalColoreadoHelper(Turtle* t, float length, int depth, int contraste){
+  if(depth == 0 || length < 5 || 0 + contraste * depth > 255 || 255  - contraste * depth < 0)
     return;
 
-  turtleSetColor(t, 0 + 35 * depth, 255 - 35 * depth , 0);
+  turtleSetColor(t, 0 + contraste * depth, 255 - contraste * depth , 0);
   turtleForward(t, length);
 
   turtleLeft(t, 30);
