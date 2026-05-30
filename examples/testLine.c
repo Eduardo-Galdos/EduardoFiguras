c0f5574e9483f148cbf5f0b5bfd51c33bef078ce
Eduardo Galdos <egaldosar@unsa.edu.pe>
2026-05-30 12:54:29 -0500

    correcion de sintaxys

--- examples/testLine.c
+++ examples/testLine.c
@@ -31,14 +31,6 @@ void CurvaDeLevy(Turtle* t, float length, int depth){
   turtleRight(t, 45);
 
 }
-
-void ArbolFractalColoreadoHelper(Turtle* t, float length, int depth){
-  if(depth == 0)
-    return;
-  int contraste = 255 / depth;
-  
-  ArbolFractalColoreadoHelper(t, length, depth, contraste);
-}
 void ArbolFractalColoreadoHelper(Turtle* t, float length, int depth, int contraste){
   if(depth == 0 || length < 5 || 0 + contraste * depth > 255 || 255  - contraste * depth < 0)
     return;
@@ -47,10 +39,10 @@ void ArbolFractalColoreadoHelper(Turtle* t, float length, int depth, int contras
   turtleForward(t, length);
 
   turtleLeft(t, 30);
-  ArbolFractalColoreado(t, length * 0.8, depth - 1);
+  ArbolFractalColoreado(t, length * 0.8, depth - 1, contraste);
 
   turtleRight(t, 60);
-  ArbolFractalColoreado(t, length * 0.8, depth - 1);
+  ArbolFractalColoreado(t, length * 0.8, depth - 1, contraste);
 
   turtlePenUp(t);
   turtleLeft(t, 30);
@@ -58,6 +50,17 @@ void ArbolFractalColoreadoHelper(Turtle* t, float length, int depth, int contras
   turtlePenDown(t);
 
 }
+
+
+void ArbolFractalColoreado(Turtle* t, float length, int depth){
+  if(depth == 0)
+    return;
+  
+  int contraste = 255 / depth;
+  
+  ArbolFractalColoreadoHelper(t, length, depth, contraste);
+}
+
 int main(void){
   TurtleApp *app = turtleAppCreate(1200, 600, "Test Line");
 
