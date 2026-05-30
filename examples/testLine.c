3ff8c6e4779db3ec061c5c81785369704d2dc108
Eduardo Galdos <egaldosar@unsa.edu.pe>
2026-05-30 12:35:53 -0500

    implementacion del arbol fractal que cambia de color dependiendo de la profundidad, testeo del caso base

--- examples/testLine.c
+++ examples/testLine.c
@@ -29,6 +29,24 @@ void CurvaDeLevy(Turtle* t, float length, int depth){
   CurvaDeLevy(t, length / sqrt(2), depth - 1);
   
   turtleRight(t, 45);
+
+}
+
+void ArbolFractalColoreado(Turtle* t, float length, int depth){
+  if(depth == 0 || length < 5 || 0 + 20 * depth > 255 || 255  - 20 * depth < 0)
+    return;
+
+  turtleSetColor(t, 0 + 20 * depth, 255 - 20 * depth , 0);
+  turtleForward(t, length);
+
+  turtleLeft(t, 30);
+  ArbolFractalColoreado(t, length * 0.8, depth - 1);
+
+  turtleRight(t, 60);
+  ArbolFractalColoreado(t, length * 0.8, depth - 1);
+
+  turtleRight(t, 30);
+  turtleBackward(t, length);
 }
 int main(void){
   TurtleApp *app = turtleAppCreate(1200, 600, "Test Line");
@@ -44,9 +62,10 @@ int main(void){
 
   turtleSetColor(t, 255, 100, 0);
   turtleSetSpeed(t, 5.0f);
-  //turtleLeft(t, 90);
+  turtleLeft(t, 90);
+  ArbolFractalColoreado(t, 100, 0);
   //arbolfractal(t, 100, 4);
-  CurvaDeLevy(t, 100, 4);
+  //CurvaDeLevy(t, 100, 4);
   turtleAppRun(app);
   turtleAppDestroy(app);
   return 0;
